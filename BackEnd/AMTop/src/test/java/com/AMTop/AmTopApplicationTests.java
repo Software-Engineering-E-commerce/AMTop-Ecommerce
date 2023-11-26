225d7ba335d9544ccda978791de01dafe5ec77b9
mahmoudattia12 <es-mahmoudattia2025@alexu.edu.eg>
2023-11-26 17:42:46 +0200

    Initialize backend

---
+++ BackEnd/AMTop/src/test/java/com/AMTop/AmTopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AMTop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmTopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
