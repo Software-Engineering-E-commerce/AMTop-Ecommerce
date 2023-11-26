cc4ea1f552ce8961bdb1eabadbb91949a6aceba9
mahmoudattia12 <es-mahmoudattia2025@alexu.edu.eg>
2023-11-27 01:48:11 +0200

    EC-46-adding the entities tables

---
+++ BackEnd/src/main/java/com/example/BackEnd/Model/User.java
@@ -0,0 +1,33 @@
+package com.example.BackEnd.Model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+@MappedSuperclass
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Column(unique = true)
+    private String email;
+    @Column
+    private String password;
+    @Column(unique = true)
+    private String gmail;
+    @Column(nullable = false, columnDefinition = "BOOLEAN DEFAULT FALSE")
+    private Boolean isGmail;
+    @Column(nullable = false, columnDefinition = "BOOLEAN DEFAULT FALSE")
+    private Boolean isVerified;
+    @Column
+    private String firstName;
+    @Column
+    private String lastName;
+    @Column
+    private String phoneNumber;
+}
