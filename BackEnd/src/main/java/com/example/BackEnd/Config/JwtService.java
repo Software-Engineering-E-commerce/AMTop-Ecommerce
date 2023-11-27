23a9b4cfab20c1f53a8c3a9b88fadbfb58b73c14
mahmoudattia12 <es-mahmoudattia2025@alexu.edu.eg>
2023-11-28 00:06:54 +0200

    EC-119-separate the secret key in the application.properties

--- BackEnd/src/main/java/com/example/BackEnd/Config/JwtService.java
+++ BackEnd/src/main/java/com/example/BackEnd/Config/JwtService.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -17,7 +18,8 @@ import java.util.function.Function;
 @Service
 public class JwtService {
 
-    private static final String SECRET_KEY = "z4T1Rd5lrunK3Bk5vy48gxlr0GzQ4Z2tvDwESHDP0X2zmaV4dml5cygRWhTB8Tee";
+    @Value("${jwt.secret-key}")
+    private String SECRET_KEY;
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
     }
