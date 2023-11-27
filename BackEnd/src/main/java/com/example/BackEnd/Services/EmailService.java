c2fa2a892c643ff8dc6cf597be81ddd324341dd0
Mohamed <mo.hassan2792@gmail.com>
2023-11-27 16:55:41 +0200

    EC-117-implement email sender service

---
+++ BackEnd/src/main/java/com/example/BackEnd/Services/EmailService.java
@@ -0,0 +1,36 @@
+package com.example.BackEnd.Services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+
+@Service
+public class EmailService {
+
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
+    @Autowired
+    private JavaMailSender emailSender;
+
+    @Async
+    public void sendEmail(String to, String subject, String body) {
+        MimeMessage message = emailSender.createMimeMessage();
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom(fromEmail);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(body, true);
+            emailSender.send(message);
+            System.out.println("Mail Send...");
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ BackEnd/src/test/java/com/example/BackEnd/Services/EmailServiceTest.java
@@ -0,0 +1,14 @@
+package com.example.BackEnd.Services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class EmailServiceTest {
+
+    @Test
+    void sendEmail() {
+    }
+}
