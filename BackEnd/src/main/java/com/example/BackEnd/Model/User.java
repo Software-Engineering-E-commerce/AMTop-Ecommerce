9efdfcbe0347013b5e840b9f8240b581eb7e8183
mahmoudattia12 <es-mahmoudattia2025@alexu.edu.eg>
2023-11-27 23:38:11 +0200

    EC-119-ApplicationConfig and JWT Filter

--- BackEnd/src/main/java/com/example/BackEnd/Model/User.java
+++ BackEnd/src/main/java/com/example/BackEnd/Model/User.java
@@ -8,12 +8,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @MappedSuperclass
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class User implements UserDetails {
     @Column(unique = true, nullable = false)
     private String email;
     @Column
@@ -28,4 +32,34 @@ public class User {
     private String lastName;
     @Column
     private String phoneNumber;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
