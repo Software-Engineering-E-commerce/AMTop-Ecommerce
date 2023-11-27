827aa4ced5b30f8272a89d7847652543e0895ec1
Adel Mahmoud <113463584+Adel-Mahmoud-Mohamed@users.noreply.github.com>
2023-11-27 22:49:00 +0200

    EC-51-A little styling

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,10 +1,27 @@
+import { useNavigate } from "react-router";
 import "./App.css";
+import BobUpWindow from "./Components/BobUpWindow";
 import SignUp from "./Pages/SignUp";
 
 function App() {
+  const navigate = useNavigate();
   return (
     <>
-      <SignUp />
+      {/* <SignUp /> */}
+      <BobUpWindow>
+        <p>
+          We've sent a verification email to you, please check it out to be able
+          to sign in
+        </p>
+        <button
+          type="button"
+          className="btn btn-primary"
+          style={{ marginLeft: 0 }}
+          onClick={() => navigate("/signup")}
+        >
+          Ok
+        </button>
+      </BobUpWindow>
     </>
   );
 }
--- FrontEnd/src/Pages/SignUp.tsx
+++ FrontEnd/src/Pages/SignUp.tsx
@@ -3,11 +3,12 @@ import Form from "../Components/Form";
 import { useNavigate } from "react-router-dom";
 
 const SignUp = () => {
-  const navigate = useNavigate()
-  
+  const navigate = useNavigate();
+
   const getSignUpCredentials = (customer: Customer, isGoogle: boolean) => {
     if (isGoogle) {
       //TODO send the request for google signup
+      handelSignUpBasicCredentials(customer);
     } else {
       //TODO send the request for basic credentials sign up
     }
@@ -15,19 +16,21 @@ const SignUp = () => {
 
   const handelSignUpBasicCredentials = async (customer: Customer) => {
     //if success then the user will be added to the DB and then routed to his home page
-    const requestPath = "";
-    try {
-      const response = await axios.post(requestPath, customer);
-
-      if (response.data === "SUCCESS") {
-        navigate("/home");
-      } else {
-        alert(response.data);
-      }
-    } catch (error) {
-      console.error("Error:", error);
-      alert(error);
-    }
+    
+    // const requestPath = "";
+    // try {
+    //   const response = await axios.post(requestPath, customer);
+    //   if (response.data === "SUCCESS") {
+    //     //TODO display a pop up window that says that the user's got an email with a verify link
+    //     // navigate("/home");
+    //   } else {
+    //     //this account already exists
+    //     // alert(response.data);
+    //   }
+    // } catch (error) {
+    //   console.error("Error:", error);
+    //   alert(error);
+    // }
   };
 
   const handelSignUpBasicGoogle = async (customer: Customer) => {
@@ -43,3 +46,4 @@ const SignUp = () => {
 };
 
 export default SignUp;
+
