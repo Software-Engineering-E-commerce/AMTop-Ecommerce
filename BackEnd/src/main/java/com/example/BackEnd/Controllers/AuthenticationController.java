5b8b11964d904994f053c297554eb20deb2e144d
Mohamed <mohamedraffeek2001@gmail.com>
2023-11-29 12:29:09 +0200

    EC-81-Testing and Unauthorized users
    added more tests, the coverage got to 100%
    on unauthorized access, the backend will now return a prober object containing a token: "Unauthorized" and a status code 403

--- BackEnd/src/main/java/com/example/BackEnd/Controllers/AuthenticationController.java
+++ BackEnd/src/main/java/com/example/BackEnd/Controllers/AuthenticationController.java
@@ -2,9 +2,9 @@ package com.example.BackEnd.Controllers;
 
 import com.example.BackEnd.DTO.AuthenticationResponse;
 import com.example.BackEnd.DTO.LoginRequest;
-import com.example.BackEnd.DTO.RegisterRequest;
 import com.example.BackEnd.Services.AuthenticationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,18 +15,15 @@ public class AuthenticationController {
 
     private final AuthenticationService service;
 
-    @PostMapping("/registerCustomer")
-    public ResponseEntity<AuthenticationResponse> register(
-            @RequestBody RegisterRequest request
-    ) {
-        return ResponseEntity.ok(service.customerRegister(request));
-    }
     @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponse> authenticate(
             @RequestBody LoginRequest request
     ) {
-        ResponseEntity<AuthenticationResponse> token = ResponseEntity.ok(service.authenticate(request));
-        return token;
+        AuthenticationResponse token = service.authenticate(request);
+        if (token.getToken().equals("Unauthorized"))
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(token);
+        else
+            return ResponseEntity.ok(token);
     }
 
 }
--- BackEnd/src/main/java/com/example/BackEnd/Services/AuthenticationService.java
+++ BackEnd/src/main/java/com/example/BackEnd/Services/AuthenticationService.java
@@ -7,11 +7,9 @@ import com.example.BackEnd.Repositories.AdminRepository;
 import com.example.BackEnd.Repositories.CustomerRepository;
 import com.example.BackEnd.DTO.AuthenticationResponse;
 import com.example.BackEnd.DTO.LoginRequest;
-import com.example.BackEnd.DTO.RegisterRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.NoSuchElementException;
@@ -23,32 +21,20 @@ public class AuthenticationService {
 
     private final AdminRepository adminRepository;
     private final CustomerRepository customerRepository;
-    private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
 
-    public AuthenticationResponse customerRegister(RegisterRequest request) {
-        Customer customer = new Customer();
-        customer.setFirstName(request.getFirstName());
-        customer.setLastName(request.getLastName());
-        customer.setEmail(request.getEmail());
-        customer.setPassword(passwordEncoder.encode(request.getPassword()));
-        customer.setIsGmail(false);
-        customer.setIsVerified(false);
-        customerRepository.save(customer);
-        var jwtToken = jwtService.generateToken(customer);
-        return AuthenticationResponse.builder()
-                .token(jwtToken)
-                .build();
-    }
-
     public AuthenticationResponse authenticate(LoginRequest request) throws NoSuchElementException {
-        authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        request.getEmail(),
-                        request.getPassword()
-                )
-        );
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            request.getEmail(),
+                            request.getPassword()
+                    )
+            );
+        } catch (Exception e) {
+            return AuthenticationResponse.builder().token("Unauthorized").build();
+        }
 
         Optional<Customer> customer = customerRepository.findByEmail(request.getEmail());
         Optional<Admin> admin = adminRepository.findByEmail(request.getEmail());
@@ -63,7 +49,9 @@ public class AuthenticationService {
                     .token(jwtToken)
                     .build();
         } else {
-            throw new NoSuchElementException();
+            return AuthenticationResponse.builder()
+                    .token("Unauthorized")
+                    .build();
         }
     }
 }
