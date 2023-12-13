95bc62e7f97e425ca04bd866bcad3331b5c164a5
Mohamed <mohamedraffeek2001@gmail.com>
2023-12-13 18:42:26 +0200

    EC-37-Implemented the frontend endpoint
    This task is finished, the only thing left is integrating with the cart component, the wishlist component, and the product page component

--- FrontEnd/src/Pages/Catalog.tsx
+++ FrontEnd/src/Pages/Catalog.tsx
@@ -1,19 +1,47 @@
+import { useLocation } from "react-router-dom";
 import ProductsList from "../Components/ProductsList";
+import axios from "axios";
 
 const Catalog = () => {
+    const location = useLocation();
+    var {userToken, from} = location.state || {};
+    userToken = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJtb2hhbWVkcmFmZmVlazIwMDFAZ21haWwuY29tIiwiaWF0IjoxNzAyNDg0OTY4LCJleHAiOjE3MDI1NzEzNjh9.KeTvuL2qceiKWeGYdEz7I5Pa4hLitngXGBv_x6lFTqA";
+    console.log(from);
+
+    const getProducts = async () => {
+      console.log("In get products");
+      try {
+        const response = await axios(
+        `http://localhost:9080/api/products/getCustomerProducts`,
+        {
+            method: "GET",
+            headers: {
+            'Authorization': `Bearer ${userToken}`,
+            },
+        }
+        );
+        const products: Product[] = response.data;
+        console.log(products)
+        return products;
+      } catch (error) {
+        console.log("Error:", error);
+        const products: Product[] = [];
+        return products;
+      }
+    };
+
     return (
         <ProductsList
-            isAdmin={false}
-            getProducts={function (): Promise<Product[]> {
+            userToken = {userToken}
+            isAdmin = {false}
+            getProducts = {getProducts}
+            addProduct = {function (): void {
                 throw new Error("Function not implemented.");
             } }
-            addProduct={function (): void {
+            removeProduct = {function (): void {
                 throw new Error("Function not implemented.");
             } }
-            removeProduct={function (): void {
-                throw new Error("Function not implemented.");
-            } }
-            updateProduct={function (): void {
+            updateProduct = {function (): void {
                 throw new Error("Function not implemented.");
             } }            
         />
