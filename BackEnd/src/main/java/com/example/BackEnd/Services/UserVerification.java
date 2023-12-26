acd1785be18d3c85af9d95959508fbc204ec9385
mahmoudattia12 <es-mahmoudattia2025@alexu.edu.eg>
2023-12-26 18:36:13 +0200

    EC-62-admin verification and complete sign up is done

--- BackEnd/src/main/java/com/example/BackEnd/Services/UserVerification.java
+++ BackEnd/src/main/java/com/example/BackEnd/Services/UserVerification.java
@@ -1,19 +1,30 @@
 package com.example.BackEnd.Services;
 
 import com.example.BackEnd.Config.JwtService;
+import com.example.BackEnd.DTO.RegisterRequest;
+import com.example.BackEnd.Model.Admin;
 import com.example.BackEnd.Model.Customer;
+import com.example.BackEnd.Repositories.AdminRepository;
 import com.example.BackEnd.Repositories.CustomerRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class UserVerification {
 
     private final JwtService jwtService;
     private final CustomerRepository CustomerRepository;
+    private final AdminRepository adminRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public void verifyUser(String token) {
         String username = jwtService.extractUsername(token);
@@ -28,6 +39,27 @@ public class UserVerification {
         }
         else {
             throw new IllegalStateException("User already verified");
+        }
+    }
+
+    public void verifyAdmin(String token, RegisterRequest request) {
+        String email = jwtService.extractUsername(token);
+        if(!email.equals(request.getEmail())){
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "The provided form mail doesn't match the verification mail sent");
+        }
+        Optional<Admin> optionalAdmin = adminRepository.findByEmail(email);
+        if (optionalAdmin.isEmpty()) {
+            throw new UsernameNotFoundException("Admin not found");
+        }
+        Admin admin = optionalAdmin.get();
+        if(!admin.getIsVerified()){
+            admin.setIsVerified(true);
+            admin.setPassword(passwordEncoder.encode(request.getPassword()));
+            admin.setFirstName(request.getFirstName());
+            admin.setLastName(request.getLastName());
+            adminRepository.save(admin);
+        }else{
+            throw new IllegalStateException("Admin already verified");
         }
     }
 }
--- BackEnd/src/test/java/com/example/BackEnd/Services/UserVerificationTest.java
+++ BackEnd/src/test/java/com/example/BackEnd/Services/UserVerificationTest.java
@@ -1,14 +1,22 @@
 package com.example.BackEnd.Services;
 
 import com.example.BackEnd.Config.JwtService;
+import com.example.BackEnd.Model.Admin;
 import com.example.BackEnd.Model.Customer;
+import com.example.BackEnd.Repositories.AdminRepository;
 import com.example.BackEnd.Repositories.CustomerRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import com.example.BackEnd.DTO.RegisterRequest;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Optional;
 
@@ -16,6 +24,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class UserVerificationTest {
 
     @Mock
@@ -23,14 +32,19 @@ class UserVerificationTest {
 
     @Mock
     private CustomerRepository customerRepository;
+    @Mock
+    private AdminRepository adminRepository;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
 
     @InjectMocks
     private UserVerification userVerification;
 
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-    }
+//    @BeforeEach
+//    void setUp() {
+//        MockitoAnnotations.openMocks(this);
+//    }
 
     @Test
     void verifyUser_WhenUserNotVerified_ShouldSetIsVerifiedTrue() {
@@ -90,5 +104,79 @@ class UserVerificationTest {
         // Verify that save method is not called
         verify(customerRepository, never()).save(any());
         assertThrows(UsernameNotFoundException.class, () -> userVerification.verifyUser(token));
+    }
+
+    @Test
+    void testVerifyAdmin_Success() {
+        // Arrange
+        String token = "mockToken";
+        String email = "admin@example.com";
+        RegisterRequest registerRequest = new RegisterRequest("mah", "att", email, "1234");
+
+        Admin admin = new Admin();
+        admin.setEmail(email);
+        admin.setIsVerified(false);
+
+        when(jwtService.extractUsername(token)).thenReturn(email);
+        when(adminRepository.findByEmail(email)).thenReturn(Optional.of(admin));
+        when(passwordEncoder.encode(any())).thenReturn("encodedPassword");
+
+        // Act
+        userVerification.verifyAdmin(token, registerRequest);
+
+        // Assert
+        verify(adminRepository, times(1)).save(any());
+    }
+
+    @Test
+    void testVerifiedAdmin() {
+        // Arrange
+        String token = "mockToken";
+        String email = "admin@example.com";
+        RegisterRequest registerRequest = new RegisterRequest("mah", "att", email, "1234");
+
+        Admin admin = new Admin();
+        admin.setEmail(email);
+        admin.setIsVerified(true);
+
+        when(jwtService.extractUsername(token)).thenReturn(email);
+        when(adminRepository.findByEmail(email)).thenReturn(Optional.of(admin));
+
+        // Act and Assert
+        IllegalStateException exception = assertThrows(IllegalStateException.class,
+                () -> userVerification.verifyAdmin(token, registerRequest));
+        assertEquals("Admin already verified", exception.getMessage());
+    }
+
+    @Test
+    void testVerifyAdmin_Conflict() {
+        // Arrange
+        String token = "mockToken";
+        String email1 = "email1", email2 = "email2";
+        RegisterRequest registerRequest = new RegisterRequest("mah", "att", email1, "1234");
+
+        when(jwtService.extractUsername(token)).thenReturn(email2);
+
+        // Act and Assert
+        ResponseStatusException exception = assertThrows(ResponseStatusException.class,
+                () -> userVerification.verifyAdmin(token, registerRequest));
+        assertEquals(HttpStatus.CONFLICT, exception.getStatusCode());
+        assertEquals("The provided form mail doesn't match the verification mail sent", exception.getReason());
+    }
+
+    @Test
+    void testVerifyAdmin_AdminNotFound() {
+        // Arrange
+        String token = "mockToken";
+        String email = "admin@example.com";
+        RegisterRequest registerRequest = new RegisterRequest("mah", "att", email, "1234");
+
+        when(jwtService.extractUsername(token)).thenReturn(email);
+        when(adminRepository.findByEmail(email)).thenReturn(Optional.empty());
+
+        // Act and Assert
+        UsernameNotFoundException exception = assertThrows(UsernameNotFoundException.class,
+                () -> userVerification.verifyAdmin(token, registerRequest));
+        assertEquals("Admin not found", exception.getMessage());
     }
 }
