c511f010b72415d607b606f24bba3761e27ea5cb
Mohamed <mo.hassan2792@gmail.com>
2023-12-27 04:47:15 +0200

    EC-140-finish sorting products with unit tests passed

--- BackEnd/src/main/java/com/example/BackEnd/Repositories/ProductRepository.java
+++ BackEnd/src/main/java/com/example/BackEnd/Repositories/ProductRepository.java
@@ -2,11 +2,25 @@ package com.example.BackEnd.Repositories;
 
 import com.example.BackEnd.Model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Optional<Product> findProductById(Long productId);
+
+    @Query("SELECT p FROM Product p LEFT JOIN p.reviews r GROUP BY p.id ORDER BY AVG(r.rating) ASC")
+    List<Product> findAllOrderByAverageRatingAsc();
+
+    @Query("SELECT p FROM Product p LEFT JOIN p.reviews r GROUP BY p.id ORDER BY AVG(r.rating) DESC")
+    List<Product> findAllOrderByAverageRatingDesc();
+
+    @Query("SELECT p FROM Product p LEFT JOIN p.reviews r GROUP BY p.id ORDER BY COUNT(*) ASC")
+    List<Product> findAllOrderByNumberOfReviewsAsc();
+
+    @Query("SELECT p FROM Product p LEFT JOIN p.reviews r GROUP BY p.id ORDER BY COUNT(*) DESC")
+    List<Product> findAllOrderByNumberOfReviewsDesc();
 }
