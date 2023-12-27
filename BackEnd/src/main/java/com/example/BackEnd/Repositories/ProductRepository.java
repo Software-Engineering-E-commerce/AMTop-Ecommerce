0a2de654c98fe87b777295164467a94784de0a2d
Mohamed <mo.hassan2792@gmail.com>
2023-12-28 00:33:56 +0200

    EC-92-implement filter of orders

--- BackEnd/src/main/java/com/example/BackEnd/Repositories/ProductRepository.java
+++ BackEnd/src/main/java/com/example/BackEnd/Repositories/ProductRepository.java
@@ -4,7 +4,6 @@ import com.example.BackEnd.Model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
