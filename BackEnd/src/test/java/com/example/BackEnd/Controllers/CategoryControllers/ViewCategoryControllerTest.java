91a47662045e8d633ef36fc027b95733a53d72fc
Mahmoud Ghlab <96776435+Mahmoudjobdis@users.noreply.github.com>
2023-12-28 08:38:15 +0200

    EC-136-Categories-Page final version of backend.

--- BackEnd/src/test/java/com/example/BackEnd/Controllers/CategoryControllers/ViewCategoryControllerTest.java
+++ BackEnd/src/test/java/com/example/BackEnd/Controllers/CategoryControllers/ViewCategoryControllerTest.java
@@ -1,15 +1,20 @@
 package com.example.BackEnd.Controllers.CategoryControllers;
 
 import com.example.BackEnd.DTO.CategoryDTO;
+import com.example.BackEnd.Model.Category;
 import com.example.BackEnd.Services.CategoryService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -83,5 +88,24 @@ public class ViewCategoryControllerTest {
         verify(request, times(1)).getHeader("Authorization");
         verify(categoryService, never()).getCategory(anyString());
         assertNull(responseEntity.getBody());
+    }
+    @Test
+    void testGetAllCategories() {
+        // Arrange
+        String authorizationHeader = "Bearer yourToken";
+        List<CategoryDTO> mockCategories = Collections.singletonList(new CategoryDTO("CategoryName", "ImageURL"));
+
+        lenient().when(request.getHeader("Authorization")).thenReturn(authorizationHeader);
+        when(categoryService.getAllCategories()).thenReturn(mockCategories);
+
+        // Act
+        ResponseEntity<List<CategoryDTO>> responseEntity = viewCategoryController.getAllCategories(authorizationHeader);
+
+        // Assert
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertEquals(mockCategories, responseEntity.getBody());
+        assertNotNull(responseEntity.getBody());
+        // Verify that extractToken method is indirectly called through request.getHeader("Authorization")
+        Mockito.verify(request, times(0)).getHeader("Authorization");
     }
 }
--- BackEnd/src/test/java/com/example/BackEnd/Services/CategoryServiceTests/EditCategoryServiceTest.java
+++ BackEnd/src/test/java/com/example/BackEnd/Services/CategoryServiceTests/EditCategoryServiceTest.java
@@ -1,121 +1,145 @@
-//package com.example.BackEnd.Services.CategoryServiceTests;
-//
-//import com.example.BackEnd.DTO.CategoryDTO;
-//import com.example.BackEnd.Model.Category;
-//import com.example.BackEnd.Repositories.CategoryRepository;
-//import com.example.BackEnd.Services.CategoryService;
-//import com.example.BackEnd.Services.ImageService;
-//import jakarta.transaction.Transactional;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//import org.springframework.web.multipart.MultipartFile;
-//
-//import java.io.IOException;
-//import java.util.NoSuchElementException;
-//import java.util.Optional;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.assertThrows;
-//import static org.mockito.ArgumentMatchers.any;
-//import static org.mockito.ArgumentMatchers.eq;
-//import static org.mockito.Mockito.*;
-//import static org.mockito.Mockito.times;
-//
-//@ExtendWith(MockitoExtension.class)
-//public class EditCategoryServiceTest {
-//    @Mock
-//    private ImageService imageService;
-//    @Mock
-//    private CategoryRepository categoryRepository;
-//    @InjectMocks
-//    private CategoryService categoryService;
-//    //-------------------------------------------------------
-//    //Test for success message
-//    @Test
-//    public void getSuccessMessageTest(){
-//        assertEquals("Category edited successfully", categoryService.getSuccessMessage(false));
-//    }
-//    //--------------------------------------------------------------------
-//    //Test for edit category successfully
-//    @Test
-//    @Transactional
-//    void editCategoryHappyCase() throws IOException{
-//        //Initiating
-//        CategoryDTO categoryDTO = CategoryDTO.builder()
-//                .name("test category")
-//                .build();
-//        MultipartFile image = mock(MultipartFile.class);
-//        when(categoryRepository.findByCategoryName(any())).thenReturn(Optional.of(new Category()));
-//        when(categoryRepository.save(any())).thenReturn(new Category());
-//        when(imageService.saveImage(any(), any(), eq(true))).thenReturn("/path/to/image");
-//        //Act
-//        categoryService.editCategory(categoryDTO,image);
-//        //Assert
-//        verify(categoryRepository, times(1)).findByCategoryName(any());
-//        verify(categoryRepository, times(1)).save(any());
-//        verify(imageService, times(1)).saveImage(any(), any(),eq(true));
-//    }
-//    //-------------------------------------------------------------
-//    //Test for editing in existent category
-//    @Test
-//    @Transactional
-//    void editCategoryDoesNotExist() throws IOException {
-//        //Initiating
-//        CategoryDTO categoryDTO = CategoryDTO.builder()
-//                .name("test category")
-//                .build();
-//        MultipartFile image = mock(MultipartFile.class);
-//        when(categoryRepository.findByCategoryName(any())).thenReturn(Optional.empty());
-//        //Act
-//        assertThrows(NoSuchElementException.class, () -> categoryService.editCategory(categoryDTO, image));
-//        //Assert
-//        verify(categoryRepository, times(1)).findByCategoryName(any());
-//        verify(categoryRepository, times(0)).save(any());
-//        verify(imageService, times(0)).saveImage(any(), any(),eq(true));
-//    }
-//    //-------------------------------------------------------
-//    //Test for editing category io exception
-//    @Test
-//    @Transactional
-//    void editCategoryIOException() throws IOException {
-//        // Initiating
-//        CategoryDTO categoryDTO = CategoryDTO.builder().name("test category").build();
-//
-//        MultipartFile image = mock(MultipartFile.class);
-//
-//        Category category = new Category();
-//        category.setCategoryName("existed category");
-//        category.setImageLink("/path/to/category/image");
-//        when(categoryRepository.findByCategoryName(any())).thenReturn(Optional.of(category));
-//        when(imageService.saveImage(any(), any(), eq(true))).thenThrow(new IOException());
-//
-//        // Act, Assert
-//        assertThrows(IOException.class, () -> categoryService.editCategory(categoryDTO, image));
-//        verify(categoryRepository, times(1)).findByCategoryName(any());
-//        verify(imageService, times(1)).saveImage(any(), any(), eq(true));
-//    }
-//
-//    //-------------------------------------------------------
-//    //Test for editing category illegal state exception
-//    @Test
-//    @Transactional
-//    void editCategoryIllegalStateException() throws IOException {
-//        // Initiating
-//        CategoryDTO categoryDTO = CategoryDTO.builder().name("test category").build();
-//
-//        MultipartFile image = mock(MultipartFile.class);
-//        Category category = new Category();
-//        category.setCategoryName("existed category");
-//        category.setImageLink("/path/to/category/image");
-//        when(categoryRepository.findByCategoryName(any())).thenReturn(Optional.of(category));
-//        when(imageService.saveImage(any(), any(), eq(true))).thenThrow(new IllegalStateException());
-//
-//        // Act, Assert
-//        assertThrows(IllegalStateException.class, () -> categoryService.editCategory(categoryDTO, image));
-//        verify(categoryRepository, times(1)).findByCategoryName(any());
-//        verify(imageService, times(1)).saveImage(any(), any(), eq(true));
-//    }
-//}
+package com.example.BackEnd.Services.CategoryServiceTests;
+
+
+import com.example.BackEnd.DTO.CategoryDTO;
+import com.example.BackEnd.Model.Category;
+import com.example.BackEnd.Repositories.CategoryRepository;
+import com.example.BackEnd.Services.CategoryService;
+import com.example.BackEnd.Services.ImageService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class CategoryServiceTest {
+    @Mock
+    private ImageService imageService;
+    @Mock
+    private CategoryRepository categoryRepository;
+    @InjectMocks
+    private CategoryService editCategoryService;
+
+    @Test
+    void testEditCategoryPart() throws IOException {
+        // Arrange
+        String originalName = "OriginalCategory";
+        CategoryDTO categoryDTO = new CategoryDTO();
+        categoryDTO.setName("NewCategory");
+        MultipartFile image = mock(MultipartFile.class);
+
+        // Mock the case where the category with the new name doesn't exist
+        when(categoryRepository.findByCategoryName(anyString())).thenReturn(Optional.empty());
+
+        // Mock the existing category
+        Category existingCategory = new Category();
+        existingCategory.setCategoryName(originalName);
+        existingCategory.setImageLink("OriginalImageLink");
+        when(categoryRepository.findByCategoryName(originalName)).thenReturn(Optional.of(existingCategory));
+
+        // Mock image service behavior
+        when(imageService.saveImage(any(), any(), anyBoolean())).thenReturn("newImageLink");
+
+        // Act
+        assertDoesNotThrow(() -> editCategoryService.editCategory(categoryDTO, image, originalName));
+
+        // Assert
+        Mockito.verify(categoryRepository).findByCategoryName(eq(originalName));
+        Mockito.verify(imageService).saveImage(eq(image), eq("NewCategory"), eq(true));
+        Mockito.verify(imageService).deleteImage(eq("OriginalImageLink"));
+        Mockito.verify(categoryRepository).delete(eq(existingCategory));
+        Mockito.verify(categoryRepository).save(any(Category.class));
+    }
+    @Test
+    void testEditCategoryImageNotEmptyAndSameName() throws IOException {
+        // Arrange
+        String originalName = "OriginalCategory";
+        CategoryDTO categoryDTO = new CategoryDTO();
+        categoryDTO.setName("OriginalCategory");  // Same name as originalName
+        MultipartFile image = mock(MultipartFile.class);
+
+        // Mock the case where the category with the new name doesn't exist
+        when(categoryRepository.findByCategoryName(anyString())).thenReturn(Optional.empty());
+
+        // Mock the existing category
+        Category existingCategory = new Category();
+        existingCategory.setCategoryName(originalName);
+        existingCategory.setImageLink("OriginalImageLink");
+        when(categoryRepository.findByCategoryName(originalName)).thenReturn(Optional.of(existingCategory));
+
+        // Mock image service behavior
+        String newImageLink = "newImageLink";
+        when(imageService.saveImage(any(), any(), anyBoolean())).thenReturn(newImageLink);
+
+        // Act
+        assertDoesNotThrow(() -> editCategoryService.editCategory(categoryDTO, image, originalName));
+
+        // Assert
+        Mockito.verify(imageService).saveImage(eq(image), eq(originalName), eq(true));
+        Mockito.verify(imageService).deleteImage(eq("OriginalImageLink"));
+        assertEquals(newImageLink, existingCategory.getImageLink());
+    }
+
+    @Test
+    void testGetAllCategories() {
+        // Arrange
+        List<Category> mockCategoryList = new ArrayList<>();
+        mockCategoryList.add(new Category("Category1", "image-link-1"));
+        mockCategoryList.add(new Category("Category2", "image-link-2"));
+
+        when(categoryRepository.findAll()).thenReturn(mockCategoryList);
+
+        // Act
+        List<CategoryDTO> result = editCategoryService.getAllCategories();
+
+        // Assert
+        assertEquals(2, result.size());
+        assertEquals("Category1", result.get(0).getName());
+        assertEquals("image-link-1", result.get(0).getImageUrl());
+        assertEquals("Category2", result.get(1).getName());
+        assertEquals("image-link-2", result.get(1).getImageUrl());
+    }
+    @Test
+    void getAllCategories_ReturnsListOfCategoryDTO() {
+        // Arrange
+        List<Category> categoryList = new ArrayList<>();
+        categoryList.add(new Category("Category1", "image-link1"));
+        categoryList.add(new Category("Category2", "image-link2"));
+
+        when(categoryRepository.findAll()).thenReturn(categoryList);
+
+        // Act
+        List<CategoryDTO> result = editCategoryService.getAllCategories();
+
+        // Assert
+        assertNotNull(result);
+        assertEquals(categoryList.size(), result.size());
+        // Add more specific assertions based on your actual implementation
+    }
+    @Test
+    void getCategory_NonExistingCategory_ReturnsNull() {
+        // Arrange
+        String categoryName = "NonExistingCategory";
+        when(categoryRepository.findByCategoryName(categoryName)).thenReturn(Optional.empty());
+
+        // Act
+        CategoryDTO result = editCategoryService.getCategory(categoryName);
+
+        // Assert
+        assertNull(result);
+    }
+
+
+}
